31d2c8865e1d2d03afa7104d4ea9005a5de59149
Sarah dePalo <sarahdepalo99@gmail.com>
2021-05-26 15:57:59 -0400

    added list function

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,36 @@
 'use strict';
+
+const generateList = document.querySelector('#generateList');
+
+const myList = document.querySelector('#myList');
+
+
+
+function grabValues() {
+    const userInput = document.querySelectorAll('input');
+    const userArray = Array.from(userInput);
+
+    // Creates the Title:
+    const titleElement = document.createElement('h2');
+    titleElement.innerText = 'My Shopping List';
+    myList.append(titleElement);
+
+    // Creates the ul 
+    const ulElement = document.createElement('ul');
+    ulElement.classList.add('listItems');
+    myList.append(ulElement);
+    userArray.forEach((item) => {
+        const liItem = document.createElement('li');
+        liItem.innerText = item.value;
+        ulElement.append(liItem);
+        console.log(liItem);
+    })
+
+}
+
+
+generateList.addEventListener('click', function() {
+    grabValues();
+}); 
+
+
